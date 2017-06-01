6a3d1383c356867b6b62878affd26f187a34c15b
EC2 Default User <ec2-user@ip-172-31-10-161.cn-north-1.compute.internal>
2017-06-02 01:14:11 +0800

    upgrade for nodejs 8.0

--- scws.cc
+++ scws.cc
@@ -13,79 +13,79 @@ Scws::~Scws() {
   scws_free(scws);
 }
 
-NAN_METHOD(Scws::addDict) {
-  NanScope();
-  Scws *scws = ObjectWrap::Unwrap<Scws>(args.This());
-  Handle<Value> arg0 = args[0];
+void Scws::addDict(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+  Nan::HandleScope scope;
+  Scws *scws = ObjectWrap::Unwrap<Scws>(info.Holder());
+  Handle<Value> arg0 = info[0];
   String::Utf8Value dict(arg0);
   if (strstr(*dict, "xdb")) {
     scws_add_dict(scws->scws, *dict, SCWS_XDICT_MEM);
   } else {
     scws_add_dict(scws->scws, *dict, SCWS_XDICT_TXT);
   }
-  NanReturnUndefined();
+  info.GetReturnValue().Set(Nan::Undefined());
 }
 
-NAN_METHOD(Scws::setDict) {
-  NanScope();
-  Scws *scws = ObjectWrap::Unwrap<Scws>(args.This());
-  Handle<Value> arg0 = args[0];
+void Scws::setDict(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+  Nan::HandleScope scope;
+  Scws *scws = ObjectWrap::Unwrap<Scws>(info.Holder());
+  Handle<Value> arg0 = info[0];
   String::Utf8Value dict(arg0);
   if (strstr(*dict, "xdb")) {
     scws_set_dict(scws->scws, *dict, SCWS_XDICT_MEM);
   } else {
     scws_set_dict(scws->scws, *dict, SCWS_XDICT_TXT);
   }
-  NanReturnUndefined();
+  info.GetReturnValue().Set(Nan::Undefined());
 }
 
-NAN_METHOD(Scws::setMulti) {
-  NanScope();
-  Scws *scws = ObjectWrap::Unwrap<Scws>(args.This());
-  int multi = args[0]->NumberValue();
+void Scws::setMulti(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+  Nan::HandleScope scope;
+  Scws *scws = ObjectWrap::Unwrap<Scws>(info.Holder());
+  int multi = info[0]->NumberValue();
   if (multi == 1) {
     scws_set_multi(scws->scws, SCWS_MULTI_SHORT);
   } else {
     scws_set_multi(scws->scws, 0);
   }
-  NanReturnUndefined();
+  info.GetReturnValue().Set(Nan::Undefined());
 }
 
-NAN_METHOD(Scws::setRule) {
-  NanScope();
-  Scws *scws = ObjectWrap::Unwrap<Scws>(args.This());
-  Handle<Value> arg0 = args[0];
+void Scws::setRule(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+  Nan::HandleScope scope;
+  Scws *scws = ObjectWrap::Unwrap<Scws>(info.Holder());
+  Handle<Value> arg0 = info[0];
   String::Utf8Value rule(arg0);
   scws_set_rule(scws->scws, *rule);
-  NanReturnUndefined();
+  info.GetReturnValue().Set(Nan::Undefined());
 }
 
-NAN_METHOD(Scws::segment) {
-  NanScope();
-    Scws *scwsObj = ObjectWrap::Unwrap<Scws>(args.This());
+void Scws::segment(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+  Nan::HandleScope scope;
+  Scws *scwsObj = ObjectWrap::Unwrap<Scws>(info.Holder());
     scws_top_t res, cur;
-    Handle<Value> arg0 = args[0];
+    Handle<Value> arg0 = info[0];
     String::Utf8Value txt(arg0);
     int txtLen = strlen(*txt);
     int limit = txtLen;
-    if (args[1]->IsNumber()) {
-      limit = args[1]->NumberValue();
+    if (info[1]->IsNumber()) {
+      limit = info[1]->NumberValue();
     }
     scws_t scws = scwsObj->scws;
     scws_send_text(scws, *txt, txtLen);
-    Local<Array> tops = NanNew<Array>();
+    Local<Array> tops = Nan::New<Array>();
     int index = 0;
 
     cur = res = scws_get_tops(scws, limit, NULL);
     while(cur != NULL){
-        Local<Object> objWord = NanNew<Object>();
-        objWord->Set(NanNew<String>("word"), NanNew<String>(cur->word));
-        objWord->Set(NanNew<String>("weight"), NanNew<Number>(cur->weight));
-        objWord->Set(NanNew<String>("times"), NanNew<Integer>(cur->times));
+        v8::Local<v8::Object> objWord = Nan::New<v8::Object>();
+        objWord->Set(Nan::New<String>("word").ToLocalChecked(), Nan::New<String>(cur->word).ToLocalChecked());
+        objWord->Set(Nan::New<String>("weight").ToLocalChecked(), Nan::New<Number>(cur->weight));
+        objWord->Set(Nan::New<String>("times").ToLocalChecked(), Nan::New<Integer>(cur->times));
         if(cur->attr[1] == '\0'){
-            objWord->Set(NanNew<String>("attr"), NanNew<String>(cur->attr, 1));
+            objWord->Set(Nan::New<String>("attr").ToLocalChecked(), Nan::New<String>(cur->attr, 1).ToLocalChecked());
         }else{
-            objWord->Set(NanNew<String>("attr"), NanNew<String>(cur->attr, 2));
+            objWord->Set(Nan::New<String>("attr").ToLocalChecked(), Nan::New<String>(cur->attr, 2).ToLocalChecked());
         }
         cur = cur->next;
         tops->Set(index, objWord);
@@ -93,33 +93,33 @@ NAN_METHOD(Scws::segment) {
     }
     scws_free_tops(res);
     scws_free_tops(cur);
-    NanReturnValue(tops);
+    info.GetReturnValue().Set(tops);
 }
 
-NAN_METHOD(Scws::serialize) {
-  NanScope();
+void Scws::serialize(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+  Nan::HandleScope scope;
 
-  Scws *scwsObj = ObjectWrap::Unwrap<Scws>(args.This());
+  Scws *scwsObj = ObjectWrap::Unwrap<Scws>(info.Holder());
   scws_t scws = scwsObj->scws;
   scws_res_t res, cur;
-  Handle<Value> arg0 = args[0];
+  Handle<Value> arg0 = info[0];
   String::Utf8Value txt(arg0);
   int txtLen = strlen(*txt);
   scws_send_text(scws, *txt, txtLen);
-  Local<Array> words = NanNew<Array>();
+  Local<Array> words = Nan::New<Array>();
   int index = 0;
   while ((res = cur = scws_get_result(scws)))
   {
     while (cur != NULL)
     {
-      Local<Object> word = NanNew<Object>();
-      word->Set(NanNew<String>("word"), NanNew<String>(*txt + cur->off, cur->len));
+      Local<Object> word = Nan::New<Object>();
+      word->Set(Nan::New<String>("word").ToLocalChecked(), Nan::New<String>(*txt + cur->off, cur->len).ToLocalChecked());
       if(cur->attr[1] == '\0'){
-        word->Set(NanNew<String>("attr"), NanNew<String>(cur->attr, 1));
+        word->Set(Nan::New<String>("attr").ToLocalChecked(), Nan::New<String>(cur->attr, 1).ToLocalChecked());
       }else{
-        word->Set(NanNew<String>("attr"), NanNew<String>(cur->attr, 2));
+        word->Set(Nan::New<String>("attr").ToLocalChecked(), Nan::New<String>(cur->attr, 2).ToLocalChecked());
       }
-      word->Set(NanNew<String>("idf"), NanNew<Number>(cur->idf));
+      word->Set(Nan::New<String>("idf").ToLocalChecked(), Nan::New<Number>(cur->idf));
       words->Set(index, word);
       //printf("WORD: %.*s/%s (IDF = %4.2f)\n", cur->len, text+cur->off, cur->attr, cur->idf);
       cur = cur->next;
@@ -129,29 +129,41 @@ NAN_METHOD(Scws::serialize) {
     scws_free_result(res);
     scws_free_result(cur);
   }
-  NanReturnValue(words);
+  info.GetReturnValue().Set(words);
 }
 
-void Scws::Init(Handle<Object> exports) {
-  NanScope();
-  Local<FunctionTemplate> tpl = NanNew<FunctionTemplate>(New);
-  tpl->SetClassName(NanNew("Scws"));
+void Scws::Init(v8::Local<v8::Object> exports) {
+  Isolate* isolate = exports->GetIsolate();
+  Nan::HandleScope scope;
+  v8::Local<v8::FunctionTemplate> tpl = Nan::New<v8::FunctionTemplate>(New);
+  tpl->SetClassName(Nan::New("Scws").ToLocalChecked());
   tpl->InstanceTemplate()->SetInternalFieldCount(1);
 
-  NODE_SET_PROTOTYPE_METHOD(tpl, "setDict", setDict);
-  NODE_SET_PROTOTYPE_METHOD(tpl, "serialize", serialize);
-  NODE_SET_PROTOTYPE_METHOD(tpl, "setRule", setRule);
-  NODE_SET_PROTOTYPE_METHOD(tpl, "setMulti", setMulti);
-  NODE_SET_PROTOTYPE_METHOD(tpl, "addDict", addDict);
-  NODE_SET_PROTOTYPE_METHOD(tpl, "segment", segment);
+  Nan::SetPrototypeMethod(tpl, "setDict", setDict);
+  Nan::SetPrototypeMethod(tpl, "serialize", serialize);
+  Nan::SetPrototypeMethod(tpl, "setRule", setRule);
+  Nan::SetPrototypeMethod(tpl, "setMulti", setMulti);
+  Nan::SetPrototypeMethod(tpl, "addDict", addDict);
+  Nan::SetPrototypeMethod(tpl, "segment", segment);
 
-  NanAssignPersistent(constructor, tpl->GetFunction());
-  exports->Set(NanNew("Scws"), tpl->GetFunction());
+  constructor.Reset(isolate, tpl->GetFunction());
+  exports->Set(String::NewFromUtf8(isolate, "Scws"),
+               tpl->GetFunction());
 }
 
-NAN_METHOD(Scws::New) {
-  NanScope();
-  Scws* obj = new Scws();
-  obj->Wrap(args.This());
-  NanReturnValue(args.This());
+void Scws::New(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+  Isolate* isolate = info.GetIsolate();
+  if (info.IsConstructCall()) {
+    Scws* obj = new Scws();
+    obj->Wrap(info.This());
+    info.GetReturnValue().Set(info.This());
+  } else {
+    const int argc = 1;
+    Local<Value> argv[argc] = { info[0] };
+    Local<Context> context = isolate->GetCurrentContext();
+    Local<Function> cons = Local<Function>::New(isolate, constructor);
+    Local<Object> result =
+        cons->NewInstance(context, argc, argv).ToLocalChecked();
+    info.GetReturnValue().Set(result);
+  }
 }
